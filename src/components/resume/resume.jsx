089631ac9bf514a2742332f75c0fd6770a7f29cd
Nathan, H <88916522+nHunter0@users.noreply.github.com>
2023-04-07 16:33:52 +1000

    Resume section done

--- src/components/resume/resume.jsx
+++ src/components/resume/resume.jsx
@@ -1,32 +1,48 @@
+import React, { useState, useEffect } from "react";
+import { Document, Page, pdfjs } from "react-pdf";
+import resumePath from "./resume-2023_downloadtest.pdf";
+import "react-pdf/dist/esm/Page/TextLayer.css";
 import "./resume.css";
 
-const resume = () => {
-  const resumePath = "../../../assets/resume-2023_downloadtest.pdf";
+// set worker path for PDF.js
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
+
+const Resume = () => {
+  const [width, setWidth] = useState(1200);
+
+  useEffect(() => {
+    setWidth(window.innerWidth);
+  }, []);
+
   return (
     <div id="resume">
       <div className="resume-container">
         <div className="resume-text">
           <p>RESUME</p>
+          <div className="pdf-controls">
+            <button
+              href={resumePath}
+              download="resume.pdf"
+              className="resume-download-btn"
+            >
+              Download
+            </button>
+          </div>
         </div>
-        <div class="pdf-viewer">
-          <object data={resumePath} type="application/pdf">
-            <p>
-              {" "}
-              SORRY, PDF of resume cannot be displayed.{" "}
-              <a href={resumePath} download>
-                Download
-              </a>{" "}
-              instead.
-            </p>
-          </object>
-        </div>
-        <div className="resume-download">
-          <a href={resumePath} download>
-            <button className="resume-download-btn"> Download </button>
-          </a>
-        </div>
+        <a href={resumePath}>
+          <div className="pdf-viewer">
+            <Document
+              file={resumePath}
+              aria-hidden="true"
+              renderTextLayer={false}
+            >
+              <Page pageNumber={1} scale={width > 1500 ? 1.5 : 1} />
+            </Document>
+          </div>
+        </a>
       </div>
     </div>
   );
 };
-export default resume;
+
+export default Resume;
