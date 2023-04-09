22b05d3e2fd62cc495ce9472dbd91f19d2a17c62
Nathan, H <88916522+nHunter0@users.noreply.github.com>
2023-04-09 11:39:10 +1000

    added footer

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,7 +1,43 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faGithub,
+  faTwitter,
+  faLinkedin,
+} from "@fortawesome/free-brands-svg-icons";
 import "./footer.css";
 
 const footer = () => {
-  return <h1>Footer</h1>;
+  return (
+    <footer className="footer">
+      <div className="social-icons">
+        <a
+          href="https://github.com/your-github-profile"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FontAwesomeIcon icon={faGithub} />
+        </a>
+        <a
+          href="https://twitter.com/your-twitter-handle"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FontAwesomeIcon icon={faTwitter} />
+        </a>
+        <a
+          href="https://www.linkedin.com/in/your-linkedin-profile/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FontAwesomeIcon icon={faLinkedin} />
+        </a>
+      </div>
+      <div className="footer-text">
+        <p>&copy; 2023 Nathan Hunter</p>
+      </div>
+    </footer>
+  );
 };
 
 export default footer;
