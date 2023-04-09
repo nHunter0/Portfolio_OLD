883a8765a4cb90f865be708cb2ae683c8b848a77
Nathan, H <88916522+nHunter0@users.noreply.github.com>
2023-04-09 14:11:47 +1000

    email working / re did the pdf view cus its fked

--- src/components/contact/contact.jsx
+++ src/components/contact/contact.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import emailjs from "emailjs-com";
 import "./contact.css";
 
@@ -7,10 +7,16 @@ const Contact = () => {
   const [email, setEmail] = useState("");
   const [subject, setSubject] = useState("");
 
-  const handleSubmit = (e) => {
+  const form = useRef();
+  const sendEmail = (e) => {
     e.preventDefault();
     emailjs
-      .sendForm("YOUR_SERVICE_ID", "YOUR_TEMPLATE_ID", e.target, "YOUR_USER_ID")
+      .sendForm(
+        "service_iuxbb5j",
+        "template_6ngs69n",
+        form.current,
+        "1Hg1Bii3Jc3WOYmsG"
+      )
       .then(
         (result) => {
           console.log(result.text);
@@ -22,12 +28,13 @@ const Contact = () => {
           console.log(error.text);
         }
       );
+    e.target.reset();
   };
 
   return (
     <section id="contact" className="contact-container">
       <h2 className="contact-text">Contact Me</h2>
-      <form className="contact-form" onSubmit={handleSubmit}>
+      <form ref={form} className="contact-form" onSubmit={sendEmail}>
         <label htmlFor="name">Name:</label>
         <input
           type="text"
@@ -58,10 +65,9 @@ const Contact = () => {
           onChange={(e) => setSubject(e.target.value)}
           required
         />
-
-        <button type="submit" className="contact-submit-btn">
-          Submit
-        </button>
+        <div className="contact-submit-btn">
+          <input type="submit" value="Submit"></input>
+        </div>
       </form>
     </section>
   );
--- src/components/resume/resume.jsx
+++ src/components/resume/resume.jsx
@@ -1,19 +1,10 @@
-import React, { useState, useEffect } from "react";
-import { Document, Page, pdfjs } from "react-pdf";
 import resumePath from "./resume-2023_downloadtest.pdf";
 import "react-pdf/dist/esm/Page/TextLayer.css";
 import "./resume.css";
 
 // set worker path for PDF.js
-pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
 const Resume = () => {
-  const [width, setWidth] = useState(1200);
-
-  useEffect(() => {
-    setWidth(window.innerWidth);
-  }, []);
-
   return (
     <div id="resume">
       <div className="resume-container">
@@ -31,14 +22,17 @@ const Resume = () => {
         </div>
 
         <div className="pdf-viewer">
-          <Document
-            file={resumePath}
-            aria-hidden="true"
-            renderTextLayer={false}
-            height="100%"
+          <object
+            data={resumePath}
+            type="application/pdf"
+            height={800}
+            width={600}
           >
-            <Page pageNumber={1} scale={width > 1500 ? 1.5 : 1} />
-          </Document>
+            <p>
+              PDF failed to download, download locally{" "}
+              <a href={resumePath}>DOWNLOAD</a>
+            </p>
+          </object>
         </div>
       </div>
     </div>
