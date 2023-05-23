6f6ba2cf5676416b26c3223ac6dd71659dc3053b
Nathan, H <88916522+nHunter0@users.noreply.github.com>
2023-05-23 20:32:20 +1000

    Updates

--- src/components/index.js
+++
@@ -1,9 +0,0 @@
-//Neater imports
-
-export { default as Navbar } from "./navbar/navbar";
-export { default as Home } from "./home/home";
-export { default as About } from "./about/about";
-export { default as Projects } from "./projects/projects";
-export { default as Resume } from "./resume/resume";
-export { default as Contact } from "./contact/contact";
-export { default as Footer } from "./footer/footer";
